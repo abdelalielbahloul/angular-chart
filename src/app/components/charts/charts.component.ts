14ebd3b9f8a39c22ed66ae21d4f19d08db820f01
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-04-15 13:18:24 +0100

    feat : add dynamic charts in dashboard component

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartsComponent implements OnInit {
 
+  @Input('my-id') myId = '';
+  @Input('my-type') myType = 'bar';
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit() : void {
+    this.barChart()
+  }
+
+  barChart() {
+    var myChart = new Chart(this.myId, {
+      type: this.myType,
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
   }
 
 }
