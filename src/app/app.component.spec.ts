be50f9143ccc6f5a488ef7dbf8d7b666dee863c5
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-04-15 12:29:01 +0100

    feat : add dashboard and charts components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-chart'`, () => {
+  it(`should have as title 'ng-statistic'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-chart');
+    expect(app.title).toEqual('ng-statistic');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-chart app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-statistic app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule
